3bf8b0d5cd0e288b395c507c7bac714bece36f05
JonLedo <jonledo.code@gmail.com>
2023-01-31 14:56:39 -0500

    updated schema to find all relevant job and company data. Updated resolvers to handle the data, and the queries from graphql using variables. React client updated, and can display all info to page

--- client/src/components/CompanyDetail.js
+++ client/src/components/CompanyDetail.js
@@ -1,20 +1,27 @@
-import { useParams } from 'react-router';
-import { companies } from '../fake-data';
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
+import { getCompany } from '../graphql/queries'
+import JobList from './JobList'
 
 function CompanyDetail() {
-  const { companyId } = useParams();
+  const [company, setCompany] = useState(null)
+  const { companyId } = useParams()
+  useEffect(() => {
+    getCompany(companyId).then(setCompany)
+  }, [companyId])
 
-  const company = companies.find((company) => company.id === companyId);
+  if (!company) {
+    return <p>Loading...</p>
+  }
+
   return (
     <div>
-      <h1 className="title">
-        {company.name}
-      </h1>
-      <div className="box">
-        {company.description}
-      </div>
+      <h1 className='title'>{company.name}</h1>
+      <div className='box'>{company.description}</div>
+      <h5 className='title is-5'>Jobs at {company.name}</h5>
+      <JobList jobs={company.jobs} />
     </div>
-  );
+  )
 }
 
-export default CompanyDetail;
+export default CompanyDetail
--- server/resolvers.js
+++ server/resolvers.js
@@ -2,13 +2,26 @@ import { Job, Company } from './db.js'
 
 export const resolvers = {
   Query: {
+    job: (_root, { id }) => {
+      return Job.findById(id)
+    },
+    company: (_root, { id }) => {
+      return Company.findById(id)
+    },
     jobs: () => Job.findAll(),
   },
 
   Job: {
     company: (job) => {
-      // console.log('resolving company for job', job)
       return Company.findById(job.companyId)
+    },
+  },
+
+  Company: {
+    jobs: (company) => {
+      return Job.findAll((job) => {
+        return job.companyId === company.id
+      })
     },
   },
 }
