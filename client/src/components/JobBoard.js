ca1733c168de049c85a9698f49589bda3f3c8b48
JonLedo <jonledo.code@gmail.com>
2023-01-31 13:25:33 -0500

    included graphql into the client side to work with. updated react code to dynamically update company name data

--- client/src/components/JobBoard.js
+++ client/src/components/JobBoard.js
@@ -1,15 +1,20 @@
-import JobList from './JobList';
-import { jobs } from '../fake-data';
+import JobList from './JobList'
+import { getJobs } from '../graphql/queries'
+import { useEffect, useState } from 'react'
 
 function JobBoard() {
+  const [jobs, setJobs] = useState([])
+  useEffect(() => {
+    getJobs().then((jobs) => setJobs(jobs))
+  }, [])
+
+  console.log(jobs)
   return (
     <div>
-      <h1 className="title">
-        Job Board
-      </h1>
+      <h1 className='title'>Job Board</h1>
       <JobList jobs={jobs} />
     </div>
-  );
+  )
 }
 
-export default JobBoard;
+export default JobBoard
